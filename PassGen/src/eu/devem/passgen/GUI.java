e5d8bd721cd6cab4406949ace24c996b45d19383
Marc Desch <marc@devem.eu>
2016-07-18 01:15:36 +0200

    Redesign Gui

--- PassGen/src/eu/devem/passgen/GUI.java
+++ PassGen/src/eu/devem/passgen/GUI.java
@@ -11,10 +11,10 @@ import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
@@ -28,7 +28,6 @@ public class GUI extends JFrame implements MouseListener, ChangeListener, ItemLi
 	
 	private JFrame frame;
 	private JPanel panel;
-	private JLabel labelPasswordLength, labelPassword;
 	private JSlider sliderPasswordLength;
 	private JCheckBox checkBoxUppercaseLetters, checkBoxLowercaseLetters, checkBoxSpecialCharacters, checkBoxNumbers;
 	private JTextField textFieldPassword;
@@ -44,7 +43,7 @@ public class GUI extends JFrame implements MouseListener, ChangeListener, ItemLi
 	
 	private void initFrame() {
 		frame = new JFrame("PassGen");
-		frame.setSize(500, 300);
+		frame.setSize(500, 250);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
@@ -54,68 +53,74 @@ public class GUI extends JFrame implements MouseListener, ChangeListener, ItemLi
 	
 	private void initPanel() {
 		panel = new JPanel();
-		panel.setBounds(0, 0, 500, 300);
+		panel.setBounds(0, 0, 500, 250);
 		panel.setOpaque(true);
 		panel.setBackground(Color.WHITE);
 		panel.setLayout(null);
 		panel.addMouseListener(this);
 		frame.add(panel);
 		
-		labelPasswordLength = new JLabel("Password length:");
-		labelPasswordLength.setBounds(0, 0, 100, 15);
-		panel.add(labelPasswordLength);
-		
-		sliderPasswordLength = new JSlider(JSlider.HORIZONTAL,0,40,0);
-		sliderPasswordLength.setBounds(0, 15, 490, 45);
+		sliderPasswordLength = new JSlider(JSlider.HORIZONTAL,0,40,20);
+		sliderPasswordLength.setBounds(10, 0, 470, 60);
 		sliderPasswordLength.setMajorTickSpacing(10);
 		sliderPasswordLength.setMinorTickSpacing(1);
 		sliderPasswordLength.setPaintTicks(true);
 		sliderPasswordLength.setPaintLabels(true);
 		sliderPasswordLength.setBackground(Color.WHITE);
 		sliderPasswordLength.addChangeListener(this);
+		sliderPasswordLength.setBorder(BorderFactory.createTitledBorder("Password length"));
+		sliderPasswordLength.setFocusable(false);
 		panel.add(sliderPasswordLength);
 		
 		checkBoxUppercaseLetters = new JCheckBox("Uppercase letters");
-		checkBoxUppercaseLetters.setBounds(0, 65, 150, 15);
+		checkBoxUppercaseLetters.setBounds(10, 65, 150, 15);
 		checkBoxUppercaseLetters.setBackground(Color.WHITE);
 		checkBoxUppercaseLetters.addItemListener(this);
+		checkBoxUppercaseLetters.setFocusable(false);
+		checkBoxUppercaseLetters.setSelected(true);
 		panel.add(checkBoxUppercaseLetters);
 		
 		checkBoxLowercaseLetters = new JCheckBox("Lowercase letters");
-		checkBoxLowercaseLetters.setBounds(0, 80, 150, 15);
+		checkBoxLowercaseLetters.setBounds(10, 80, 150, 15);
 		checkBoxLowercaseLetters.setBackground(Color.WHITE);
 		checkBoxLowercaseLetters.addItemListener(this);
+		checkBoxLowercaseLetters.setFocusable(false);
+		checkBoxLowercaseLetters.setSelected(true);
 		panel.add(checkBoxLowercaseLetters);
 		
 		checkBoxSpecialCharacters = new JCheckBox("Special characters");
-		checkBoxSpecialCharacters.setBounds(0, 95, 150, 15);
+		checkBoxSpecialCharacters.setBounds(10, 95, 150, 15);
 		checkBoxSpecialCharacters.setBackground(Color.WHITE);
 		checkBoxSpecialCharacters.addItemListener(this);
+		checkBoxSpecialCharacters.setFocusable(false);
+		checkBoxSpecialCharacters.setSelected(true);
 		panel.add(checkBoxSpecialCharacters);
 		
 		checkBoxNumbers = new JCheckBox("Numbers");
-		checkBoxNumbers.setBounds(0, 110, 150, 15);
+		checkBoxNumbers.setBounds(10, 110, 150, 15);
 		checkBoxNumbers.setBackground(Color.WHITE);
 		checkBoxNumbers.addItemListener(this);
+		checkBoxNumbers.setFocusable(false);
+		checkBoxNumbers.setSelected(true);
 		panel.add(checkBoxNumbers);
 		
-		labelPassword = new JLabel("Password:");
-		labelPassword.setBounds(0, 150, 100, 15);
-		panel.add(labelPassword);
-		
 		textFieldPassword = new JTextField();
-		textFieldPassword.setBounds(0, 165, 500, 20);
+		textFieldPassword.setBounds(10, 130, 470, 40);
 		textFieldPassword.setEditable(false);
+		textFieldPassword.setBackground(Color.WHITE);
+		textFieldPassword.setBorder(BorderFactory.createTitledBorder("Password"));
 		panel.add(textFieldPassword);
 		
 		buttonRegenerate = new JButton("Regenerate");
-		buttonRegenerate.setBounds(0, 190, 150, 20);
+		buttonRegenerate.setBounds(10, 180, 150, 20);
 		buttonRegenerate.addActionListener(this);
+		buttonRegenerate.setFocusable(false);
 		panel.add(buttonRegenerate);
 		
 		buttonCopyToClipboard = new JButton("Copy to clipboard");
-		buttonCopyToClipboard.setBounds(160, 190, 150, 20);
+		buttonCopyToClipboard.setBounds(170, 180, 150, 20);
 		buttonCopyToClipboard.addActionListener(this);
+		buttonCopyToClipboard.setFocusable(false);
 		panel.add(buttonCopyToClipboard);
 		
 		panel.repaint();
